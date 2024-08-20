4e6f7878d51fae343366203e205fcf640946b2e5
zaeemali272 <zaeemali272@gmail.com>
2024-08-20 19:18:24 +0500

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
-import { ProjectSliderComponent } from '../../components/project-slider/project-slider.component';
-import { FooterComponent } from '../../components/footer/footer.component';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GalleriaModule } from 'primeng/galleria';
+
+
+
+interface GalleriaImage {
+  URL: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProjectSliderComponent, FooterComponent],
+  imports: [GalleriaModule,],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  
+  img: GalleriaImage[] = [];
+
+  ngOnInit() {
+    this.img = [
+      {
+        URL: 'assets/front-1.png',
+      },
+      {
+        URL: 'assets/front-0.png',
+      },
+      {
+        URL: 'assets/front-2.png',
+      },
+      {
+        URL: 'assets/front-3.png',
+      },
+    ];
+  }
 }
